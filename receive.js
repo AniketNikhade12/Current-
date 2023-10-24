f95006bff6aea28be9c303c45298f850cd45aebd
AniketNikhade12 <aniket.nikhade2001@gmail.com>
2023-10-24 10:57:43 +0530

    first

---
+++ receive.js
@@ -0,0 +1,5 @@
+console.log("HEllo World");
+// import {a} from "./send.js"
+// console.log(a);
+import {obj} from "./send.js"
+   console.log(obj);
---
+++ send.js
@@ -0,0 +1,7 @@
+// export var a =12
+
+export var obj =
+[
+    {name:"aniket",since:2001}
+]
+    console.log(obj);
